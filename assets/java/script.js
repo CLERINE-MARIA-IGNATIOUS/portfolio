a1f71e70113f25efefdb6a83708f33ea2fa1f8b2
CLERINE MARIA <clerinemariaignatious@gmail.com>
2022-02-03 23:54:32 +0530

    initial commit

---
+++ assets/java/script.js
@@ -0,0 +1,15 @@
+// const button=document.getElementById('btnID');
+// const clickHandler=() =>{};
+// button.addEventListener('click',()=> {
+//     const para=document.createElement("p");
+//     para.innerHTML="Thank you"
+//     document.body.print(para)
+
+
+// });
+
+function display(){
+    x="THANK YOU \n THE END"
+    // document.getElementById("display-head").innerHTML=x
+    alert(x)
+}
